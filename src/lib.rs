7cf311aac45c8d2e76a630916018221f265749b7
smartinez <smartinez@nuclearis.com>
2021-02-22 16:46:05 -0300

    feat: add assert

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,13 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub fn convert_u8_to_ascii(input: &u8) -> Option<u8> {
-    match 48u8.checked_add(*input) {
-        Some(val) => Some(val),
-        None => None,
-    }
+/// Utility function that converts a u8 integer to a ASCII byte
+/// The addition does not check for a possible overflow, since the u8 numbers provided are less than 9
+pub fn convert_u8_to_ascii(input: &u8) -> u8 {
+    assert!(input < &9u8, "Number has to be less than 10");
+    48u8 + input
 }
 
 #[test]
 fn add_48_to_existing_number() {
-    assert_eq!(convert_u8_to_ascii(&20), Some(68));
-}
-
-#[test]
-fn check_for_overflow() {
-    assert_eq!(convert_u8_to_ascii(&240), None);
+    assert_eq!(convert_u8_to_ascii(&4), 52);
 }
